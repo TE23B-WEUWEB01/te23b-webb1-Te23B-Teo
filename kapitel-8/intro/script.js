8f2c2922376394e3aba987f06b7aad2db565415b
teo erfass josefsson <teo.erfassjosefsson@elev.ga.ntig.se>
2025-03-13 11:08:25 +0100

    tränat på java script, variabler

---
+++ kapitel-8/intro/script.js
@@ -0,0 +1,13 @@
+// var tal = 65;
+// var tal1 = 12;
+// var tal2 = 8;
+// var summa = tal1+tal2*tal;
+// alert(tal1 + " + " + tal2 + " * " +  tal + " = " + summa);
+
+var förnamn = "teo";
+var efternamn = "hopps";
+var helaNamnet = förnamn + " " + efternamn
+var hobby = "spela";
+var kostnad = 1000
+var mening = helaNamnet + "'s" + " hobby är " + hobby + ".\nMin hobby kostar " + kostnad + " kr/år.";
+alert(mening);
