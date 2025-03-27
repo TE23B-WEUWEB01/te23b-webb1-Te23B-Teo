f5fdad36f8c3532b216e3107806ccb4455e1258f
teo erfass josefsson <teo.erfassjosefsson@elev.ga.ntig.se>
2025-03-27 11:08:16 +0100

    Tränat på onclick, funktioner, queryselector()

---
+++ kapitel-8/interaktivitet/script.js
@@ -0,0 +1,14 @@
+function Darkpage() {
+ document.body.style = "background: #000; color: #fff;"
+ document.querySelector("h1").style = "font-size: 75px;"
+}
+function Whitepage() {
+    document.body.style = "background: #fff; color: #000;"
+     document.querySelector("h1").style = "font-size: 50px;"
+}
+function Rubrik() {
+document.querySelector("h1").style = "font-size: 80px; rotate: 330deg; color: fuchsia; font-variant-caps: small-caps;  text-shadow: 5px 5px 5px cyan;"
+}
+function Låttext() {
+    document.querySelector("p").style = "visibility: visible;"
+    }
